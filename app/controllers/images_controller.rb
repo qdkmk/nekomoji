b946e0d3fe882c2d04af7479e428791e1d5f9e6c
qdkmk <qdokuhon@gmail.com>
2019-08-01 15:30:29 +0900

    delete to_blob controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,10 @@
 class ImagesController < ApplicationController
   def ogp
     text = ogp_params[:text]
-    image = OgpCreator.build(text).tempfile.open.read
+    @image = OgpCreator.build(text).tempfile.open.read
     #send_data image, :type => 'image/png',:disposition => 'inline'
     #send_data(image.to_blob, :type => 'image/png',:disposition => 'inline')
-    @image = image.to_blob
+    #@image = image.to_blob
   end
 
   def index
